3ce4ca82241ff956ef1a8974bff693ea8c2faad1
Srajan Kushwaha <srajank2003@gmail.com>
2025-06-08 21:31:00 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use("/api/contacts",require("./routes/contactRoutes"));
+
+
+
+app.listen(port, () => {
+    console.log(`Server started at port: ${port}`);
+})
+
