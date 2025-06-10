8fec6f4b4d4d67bc4cd24e23e83ec6ac4fa806fc
Srajan Kushwaha <srajank2003@gmail.com>
2025-06-10 22:58:34 +0530

    user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,20 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../models/user.model");
 
 const registerUser = asyncHandler(async (req, res) => {
+    const {username, email, password} = req.body;
+
+    if(!username || !email || !password){
+        res.status(400);
+        throw new Error("All fields are mandatory");
+    }
+
+    const userAvailable = await User.findOne({email});
+    if(userAvailable){
+        res.status(400);
+        throw new Error("User already registered!");
+    }
+
     res.json({ message: "Register the user"});
 });
 
